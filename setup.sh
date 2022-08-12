469f7508338fed6b9b139bb83bbe6d8c443cbaa6
Johannes Egger <j.egger@posteo.at>
2022-08-12 22:23:25 +0200

    Copy python script when installing service

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo mkdir -p /opt/ohmcontrol && sudo cp app.py /opt/ohmcontrol
 sudo cp app.service /etc/systemd/system/ohmcontrol.service
 sudo systemctl daemon-reload && sudo systemctl enable ohmcontrol && sudo systemctl start ohmcontrol --no-block
