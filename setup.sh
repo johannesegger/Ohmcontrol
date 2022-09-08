d06ed1f8fbf6171a312a628975a9929875e09281
Johannes Egger <j.egger@posteo.at>
2022-09-08 07:18:12 +0200

    Copy all source files when installing service

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 sudo apt update && sudo apt install -y python3-pip
 sudo pip install pyserial
 
-sudo mkdir -p /opt/ohmcontrol && sudo cp app.py /opt/ohmcontrol
+sudo mkdir -p /opt/ohmcontrol && sudo cp app.py fn.py pwm.py /opt/ohmcontrol
 sudo cp app.service /etc/systemd/system/ohmcontrol.service
 sudo systemctl daemon-reload && sudo systemctl enable ohmcontrol && sudo systemctl start ohmcontrol --no-block
